dc9c765e163d6922666447a8a031e8ec5a692773
britthubs <britt.van.schoors@gmail.com>
2024-12-04 21:47:31 +0100

    Add comments for readability

--- efficiency.py
+++ efficiency.py
@@ -1,18 +1,15 @@
 import xraylib
 import numpy as np
-import matplotlib.pyplot as plt
-
-energies = np.arange(1, 500, 1) # x ranges from 1 -> 500 stepsize 1
 
 def quantum_ef(z_w, z_c, dens_w, thck_w, dens_c, thck_c, energies): 
     qe_list = []
     for i in energies:
-        e = int(i)
-        att = xraylib.CS_Total(z_w, e) # mass attenuation window (cm^2 /g)
-        lin_w = att * dens_w # linear attenuation coef window (1/cm)
-        att = xraylib.CS_Total(z_c, e) # mass attenuation crystal (cm^2 /g)
-        lin_c = att * dens_c # linear attenuation coef crystal (1/cm)
-        qe = np.exp(-lin_w* thck_w *10**(-1)) * (1 - np.exp(-lin_c * thck_c *10**(-1)))
+        e = int(i) 
+        att = xraylib.CS_Total(z_w, e) # mass attenuation coefficient of window material(cm^2 /g)
+        lin_w = att * dens_w # linear attenuation coefficient of the window material (1/cm)
+        att = xraylib.CS_Total(z_c, e) # mass attenuation coefficient of the crystal material (cm^2 /g)
+        lin_c = att * dens_c # linear attenuation coefficient of the crystal material(1/cm)
+        qe = np.exp(-lin_w* thck_w *10**(-1)) * (1 - np.exp(-lin_c * thck_c *10**(-1))) # estimation of the quantum efficiency using the formula
         qe_list.append(qe)
     return(qe_list)
 
