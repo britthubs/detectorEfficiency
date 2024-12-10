74d4a6c3aa4e0fe43f5591bc1ca763c5baace657
britthubs <britt.van.schoors@gmail.com>
2024-12-10 16:26:24 +0100

    Add dead-layer assumption, change log scale axes, minor changes, add structure to .py file

--- efficiency.py
+++ efficiency.py
@@ -1,30 +1,29 @@
 import xraylib
 import numpy as np
 import matplotlib.pyplot as plt
-import numpy as np
+import matplotlib.ticker as ticker
 
-energies = np.arange(1, 500, 1) # x ranges from 1 -> 500 with stepsize 1
+energies = np.arange(0.1, 200, 0.01)  # x (energies) ranges from 0.1 to 200 keV with stepsize 0.01
 
 def quantum_ef(z_w, z_c, dens_w, thck_w, dens_c, thck_c, energies): 
     qe_list = []
-    for i in energies:
-        e = int(i) 
+    for energy in energies:
         # window parameters
-        att_w = xraylib.CS_Total(z_w, e) # mass attenuation coefficient of window material(cm^2 /g)
-        lin_w = att_w * dens_w # linear attenuation coefficient of the window material (1/cm)
-        window = lin_w* thck_w
+        att_w = xraylib.CS_Total(z_w, energy)  # mass attenuation coefficient of window material(cm^2 /g)
+        lin_w = att_w * dens_w  # linear attenuation coefficient of the window material (1/cm)
+        window = lin_w * (thck_w * (10**(-4)))
         
         # crystal parameters
-        att_c = xraylib.CS_Total(z_c, e) # mass attenuation coefficient of the crystal material (cm^2 /g)
-        lin_c = att_c * dens_c # linear attenuation coefficient of the crystal material(1/cm)
-        crystal = lin_c * thck_c *10**(-1)
+        att_c = xraylib.CS_Total(z_c, energy)  # mass attenuation coefficient of the crystal material (cm^2 /g)
+        lin_c = att_c * dens_c  # linear attenuation coefficient of the crystal material(1/cm)
+        crystal = lin_c * thck_c * 10**(-1)
         
         # dead layer parameters
-        dead_layer = lin_c * 0.15 * 10**(-4) 
+        dead_layer = lin_c * (0.15 * (10**(-4)))
         
-        qe = np.exp(-window -dead_layer) * (1 - np.exp(-crystal)) # estimation of the quantum efficiency using the formula
+        qe = np.exp(-window - dead_layer) * (1 - np.exp(-crystal))  # estimation of the quantum efficiency using the formula
         qe_list.append(qe)
-    return(qe_list)
+    return qe_list
 
 """
 z_w = atomic number of window element (for example Be would be 4)
@@ -42,18 +41,27 @@ thck_c = thickness of the crystal (mm)
 energies = array of energies for example from 1 to 500 keV with stepsize 1 -> np.arange(1, 500, 1)
 """
 
+# Plots
+plt.plot(energies, quantum_ef(z_w=4, z_c=14, dens_w=1.848, thck_w=25, 
+                              dens_c=2.329, thck_c=1, energies=energies), label="SDD", linewidth=1)
+plt.plot(energies, quantum_ef(z_w=4, z_c=32, dens_w=1.848, thck_w=125, 
+                              dens_c=5.323, thck_c=6, energies=energies), label="HPGe", linewidth=1)
 
-
-# plots two data sets (2 detectors), add more if you want
-plt.plot(energies, quantum_ef(z_w=4, z_c=14, dens_w=1.848, thck_w=0.025, 
-                              dens_c=2.33, thck_c=1, energies=energies), label="Si detector")
-plt.plot(energies, quantum_ef(z_w=4, z_c=32, dens_w=1.848, thck_w=0.125, 
-                              dens_c=5.323, thck_c=6, energies=energies), label="Ge detector")
-# plt.axvline(x=30)
-plt.xlabel("Energy [KeV]")
-plt.ylabel("$\epsilon$")
-plt.title("") # edit title
+# Set labels and scaling
+ax = plt.gca()  # get axes
+plt.title("")  # edit title
 plt.legend()
-plt.grid(True)
+
+# x-axis formatting
+plt.xlabel("Energy [KeV]")
 plt.xscale('log')
+plt.xlim((0.5, 200))
+ax.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, _: '{:g}'.format(x)))  # show 1 instead of 10^0 etc
+
+# y-axis formatting
+plt.ylabel("$\epsilon$")
+plt.yscale('log')
+plt.ylim((0.01, 1))
+ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y, _: '{:g}'.format(y)))  # show 1 instead of 10^0 etc
+
 plt.show()
